0ba62a1453f67385522cebbb77b53883b3122128
yutianw <yutianw20@gmail.com>
2022-02-19 15:45:19 -0500

    finished specification for the util Pos class

--- src/main/java/game/utils/Pos.java
+++ src/main/java/game/utils/Pos.java
@@ -1,6 +1,10 @@
 package game.utils;
 
-
+/**
+ * A utilization class to represent the 2D positions on the board,
+ * with basic getters and setters, and also a function to decide whether
+ * two {@link Pos}es are equals
+ */
 public class Pos {
     private int row;
     private int col;
@@ -34,14 +38,11 @@ public class Pos {
         }
 
         Pos pos = (Pos) obj;
-
         return (pos.getCol() == this.col && pos.getRow() == this.row);
     }
 
     @Override
     public String toString() {
-        return "Pos{" + row +
-                ", " + col +
-                '}';
+        return "Pos{" + row + ", " + col + '}';
     }
 }
