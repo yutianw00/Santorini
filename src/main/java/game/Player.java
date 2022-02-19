4b753b1086b07198f556a44bd7fc6ebc1cde8c82
yutianw <yutianw20@gmail.com>
2022-02-19 15:41:18 -0500

    finished specification for the Player interface

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -3,8 +3,36 @@ package game;
 import game.utils.Pos;
 
 public interface Player {
+
+    /**
+     * Set the first worker for the player
+     * Effect: update that worker's position
+     *
+     * @param pos The updated {@link Pos} position of that worker
+     * @return Whether the position is a valid position and
+     * the update is successfully performed
+     */
     boolean setWorkerA(Pos pos);
+
+    /**
+     * Set the second worker for the player
+     * Effect: update that worker's position
+     *
+     * @param pos The updated {@link Pos} position of that worker
+     * @return Whether the position is a valid position and
+     * the update is successfully performed
+     */
     boolean setWorkerB(Pos pos);
+
+    /**
+     * Get the first worker of the player
+     * @return The first {@link Worker} of the player
+     */
     Worker getWorkerA();
+
+    /**
+     * Get the second worker of the player
+     * @return The second {@link Worker} of the player
+     */
     Worker getWorkerB();
 }
