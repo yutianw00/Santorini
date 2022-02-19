72ee51d8c3b782cc6848bd475a01d969c6a6aa7b
yutianw <yutianw20@gmail.com>
2022-02-19 15:43:41 -0500

    finished specification for the Worker interface

--- src/main/java/game/Worker.java
+++ src/main/java/game/Worker.java
@@ -3,6 +3,19 @@ package game;
 import game.utils.Pos;
 
 public interface Worker {
+    /**
+     * Move the worker to the corresponding position
+     *
+     * Prerequisite: {@code checkMove()} of the {@link Board} class is
+     * performed with a return value of {@code true}
+     *
+     * @param pos The {@link Pos} position that the worker is moving to
+     */
     void move(Pos pos);
+
+    /**
+     * Get the current position of the worker
+     * @return The current {@link Pos} position of the worker
+     */
     Pos getPos();
 }
