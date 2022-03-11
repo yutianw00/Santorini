23fa73a854fa9ccfad6ea98e52365f2777087b5d
yutianw <yutianw20@gmail.com>
2022-03-10 22:58:16 -0500

    update the PlayerTest.java test cases to make the test align with the new APIs

--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -15,26 +15,17 @@ public class PlayerTest {
         player = new PlayerImpl();
     }
 
-//    @Test
-//    public void setWorkerATest() {
-//        assertTrue(player.setWorkerA(new Pos(3,4)));
-//        assert(player.getWorkerA().getPos().equals(new Pos(3,4)));
-//    }
+    @Test
+    public void setWorkerATest() {
+        player.setWorkerA(new Pos(3,4));
+        assert(player.getWorkerA().getPos().equals(new Pos(3,4)));
+    }
 
-//    @Test
-//    public void setWorkerATestInvalidInput1() {
-//        assertFalse(player.setWorkerA(new Pos(4,5)));
-//    }
-//
-//    @Test
-//    public void setWorkerATestInvalidInput2() {
-//        assertFalse(player.setWorkerA(new Pos(-1,0)));
-//    }
-//
-//    @Test
-//    public void setWorkerB() {
-//        assertTrue(player.setWorkerB(new Pos(0,0)));
-//        assert(player.getWorkerB().getPos().equals(new Pos(0,0)));
-//    }
+
+    @Test
+    public void setWorkerB() {
+        player.setWorkerB(new Pos(0,0));
+        assert(player.getWorkerB().getPos().equals(new Pos(0,0)));
+    }
 
 }
