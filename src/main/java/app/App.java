4a5093063c5af2d00860cc6644a79a2c366a79d8
yutianw <yutianw20@gmail.com>
2022-03-23 21:49:05 -0400

    initial app class to listen to connections from the frontend with a testing url for testing connection

---
+++ src/main/java/app/App.java
@@ -0,0 +1,60 @@
+package app;
+
+import java.io.IOException;
+import java.util.Map;
+import fi.iki.elonen.NanoHTTPD;
+import game.Game;
+import game.impl.GameImpl;
+
+public class App extends NanoHTTPD {
+
+    public static void main(String[] args) {
+        try {
+            new App();
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
+        }
+    }
+
+    private Game game;
+
+    public App() throws IOException {
+        super(8080);
+
+        this.game = new GameImpl();
+
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        System.out.println("\nRunning!\n");
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String uri = session.getUri();
+        Map<String, String> params = session.getParms();
+        if (uri.equals("/test")) {
+            System.out.println("Testing connection: server side received!");
+            String myjsonstr = "{ \"test1\": \"" + 1 + "\"," +
+                    " \"test2\": \"" + 2 + "\"," +
+                    " \"test3\": \"" + 3 + "\"}";
+
+            return newFixedLengthResponse(myjsonstr);
+        }
+        return null;
+//        if (uri.equals("/newgame")) {
+//            this.game = new Game();
+//        } else if (uri.equals("/play")) {
+//            this.game = this.game.play(Integer.parseInt(params.get("x")), Integer.parseInt(params.get("y")));
+//        } else if (uri.equals("/undo")) {
+//            this.game = this.game.undo();
+//        }
+//        // Extract the view-specific data from the game and apply it to the template.
+//        GameState gameplay = GameState.forGame(this.game);
+//        return newFixedLengthResponse(gameplay.toString());
+    }
+
+    public static class Test{
+        public String getText() {
+            return "Hello World!";
+        }
+    }
+}
